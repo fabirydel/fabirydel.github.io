de741ce34ff649ce373885896173b6c2a93c5e7b
Fabian Rydel <fabirydel@gmail.com>
2020-05-18 22:41:29 -0300

    Modal animation

--- assets/js/gallery_modal.js
+++ assets/js/gallery_modal.js
@@ -1,12 +1,19 @@
 // Open the Modal
 function openModal(index) {
   document.getElementById("myModal").style.display = "block";
+  setTimeout(function() {
+    document.getElementById("myModal").style.visibility = "visible";
+    document.getElementById("myModal").style.opacity = "1";
+  }, 10);
+
   currentSlide(index)
 }
 
 // Close the Modal
 function closeModal() {
-  document.getElementById("myModal").style.display = "none";
+  document.getElementById("myModal").style.visibility = "hidden";
+  document.getElementById("myModal").style.opacity = "0";
+  setTimeout(function() { document.getElementById("myModal").style.display = "none"; }, 250);
 }
 
 function ignore(e) {
@@ -17,8 +24,9 @@ var slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
-function plusSlides(n) {
+function plusSlides(event, n) {
   showSlides(slideIndex += n);
+  ignore(event)
 }
 
 // Thumbnail image controls
@@ -35,9 +43,6 @@ function showSlides(n) {
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
   }
   if (slides[slideIndex-1]) slides[slideIndex-1].style.display = "block";
   if (dots[slideIndex-1]) {
