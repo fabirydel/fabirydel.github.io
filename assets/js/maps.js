6f9fa9e350064e7902543fb203d152db82427103
Fabian Rydel <fabirydel@gmail.com>
2020-12-29 10:55:12 +0100

    uruguay a todo

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -2,18 +2,38 @@ document.addEventListener("DOMContentLoaded", function() {
   country = document.getElementById('country-map');
   if (!country) return
 
-  var mapcountryMap = L.map('country-map', {
-    maxBounds: JSON.parse(country.dataset.bounds),
-  }).setView(JSON.parse(country.dataset.latlng), country.dataset.zoom);
+  var mapboxAttribution = 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+  var mapBoxUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}'
+  var accessToken = 'pk.eyJ1IjoiZmFiaXJ5ZGVsIiwiYSI6ImNrajYwcHcwbTYzZDEycWxicjFtNzdqa2kifQ.8AEeHc6W_kHGvgIZQNq2WA'
 
-  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  var outdoors = L.tileLayer(mapBoxUrl, {
+    attribution: mapboxAttribution,
     minZoom: country.dataset.zoom,
     id: 'mapbox/outdoors-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiZmFiaXJ5ZGVsIiwiYSI6ImNrajYwcHcwbTYzZDEycWxicjFtNzdqa2kifQ.8AEeHc6W_kHGvgIZQNq2WA'
-  }).addTo(mapcountryMap);
+    accessToken: accessToken
+  });
+  var satellite = L.tileLayer(mapBoxUrl, {
+    attribution: mapboxAttribution,
+    minZoom: country.dataset.zoom,
+    id: 'mapbox/satellite-streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: accessToken
+  });
+
+  var baseMaps = {
+    'Outdoors': outdoors,
+    'Satellite': satellite
+  };
+
+  var countryMap = L.map('country-map', {
+    maxBounds: JSON.parse(country.dataset.bounds),
+    layers: [satellite]
+  }).setView(JSON.parse(country.dataset.latlng), country.dataset.zoom);
+
+  L.control.layers(baseMaps).addTo(countryMap);
 
   for (i = 1; i <= country.dataset.cities; i++) {
     cityImg = country.dataset[`img-${i}`]
@@ -32,7 +52,7 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     let marker = L.marker(cityLatLng, { riseOnHover: true, path: `/cities/${cityPath}`, icon: myIcon })
-    marker.addTo(mapcountryMap).on('click', () =>
+    marker.addTo(countryMap).on('click', () =>
       setTimeout(function() {
         window.location.href = marker.options.path;
       }, 0)
